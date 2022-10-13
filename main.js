6dd282f340301b1c25e6682d41192893316b9dc5
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-13 20:04:53 +0600

    Making Windows For Cross Platform

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+//NODE_ENV
+const isDev = process.env.NODE_ENV !== "production";
+
+// For Cross Platfrom
+const isWindows = process.platform === "win32";
+const isMac = process.platform === "darwin";
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "gRio Image Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  // Open Dev Tools in Dev Mode
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isWindows || !isMac) {
+    app.quit();
+  }
 });
